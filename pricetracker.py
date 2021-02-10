c0549bfc637652c41aa0a9fbbed0c639367b7782
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-02-10 19:09:02 +0200

    started pricetracker

--- pricetracker.py
+++ pricetracker.py
@@ -1,3 +1,10 @@
+import yfinance as yf
+from cls import cls
+
 def pricetracker(stock):
-    print()
-    #still in work
+    while True:
+        tickerdata = yf.Ticker(stock)
+        tickerinfo = tickerdata.info
+        cls()
+        print(tickerinfo.get("symbol") + " " + tickerinfo.get('shortName') + " " + str(tickerinfo.get('regularMarketPrice')))
+#not finished
