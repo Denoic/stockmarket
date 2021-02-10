0d75634523f7b85018e706f0fb150b3da29a4395
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-02-10 06:42:00 +0200

    made multiple files for more organization

--- project.py
+++ project.py
@@ -5,77 +5,10 @@ from rich.console import Console
 from rich.markdown import Markdown
 from rich import print
 import json
-
-def filecreation(stock):
-    url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-detail"
-
-    querystring = {"symbol": stock, "region": "US"}
-
-    headers = {
-        'x-rapidapi-key': "623ecea589msh70f225267987d8ep18f5c1jsn34e5895911b9",
-        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
-    }
-
-    response = requests.request(
-        "GET", url, headers=headers, params=querystring)
-
-    result = response.json()
-        
-    f = open("info.md","w+")
-    f.write(result.get("summaryProfile").get("longBusinessSummary"))
-    f.close()
+from info import Info
 
 def cls(): return os.system('cls')
 
-def BusinessSummary():
-    console = Console()
-    with open("info.md") as info:
-        markdown = Markdown(info.read())
-    console.print("[bold red]Business Summary:[/bold red]")
-    console.print(markdown)
-    os.remove('info.md')
-
-def Info(stock):
-    url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-detail"
-
-    querystring = {"symbol": stock, "region": "US"}
-
-    headers = {
-        'x-rapidapi-key': "623ecea589msh70f225267987d8ep18f5c1jsn34e5895911b9",
-        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
-    }
-
-    response = requests.request(
-        "GET", url, headers=headers, params=querystring)
-
-    result = response.json()
-    print("[bold red]Name:[/bold red]")
-    print(result.get("price").get("longName"))
-    print()
-    print("[bold red]Sector:[/bold red]")
-    print(result.get("summaryProfile").get("sector"))
-    print()
-    print("[bold red]Industry:[/bold red]")
-    print(result.get("summaryProfile").get("industry"))
-    print()
-    print("[bold red]Zip:[/bold red]")
-    print(result.get("summaryProfile").get("zip"))
-    print()
-    print(BusinessSummary())
-    print()
-    print("[bold red]City:[/bold red]")
-    print(result.get("summaryProfile").get("city"))
-    print()
-    print("[bold red]State:[/bold red]")
-    print(result.get("summaryProfile").get("state"))
-    print()
-    print("[bold red]Country:[/bold red]")
-    print(result.get("summaryProfile").get("country"))
-    print()
-    print("[bold red]Website:[/bold red]")
-    print(result.get("summaryProfile").get("website"))
-    print()
-    
 def pricetracker(stock):
     print()
                 
@@ -115,7 +48,6 @@ while True:
         InpStock = input("Enter ticker: ")
         InpStock = InpStock.upper()
         cls()
-        filecreation(InpStock)
         Info(InpStock)
     
     elif choice1.lower() == "price":
