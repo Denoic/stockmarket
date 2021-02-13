489681b44f8a46ae5f75788590eaae6adf67b598
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-02-13 12:32:25 +0200

    changed name and added exit function

---
+++ MAIN.py
@@ -0,0 +1,45 @@
+import keyboard
+import json
+from info import Info
+from pricetracker import pricetracker
+from trending import trends
+from news import news
+from info import filecreation
+from cls import cls
+from exit import exit
+
+while True:
+    choice = ""
+    cls()
+    print("Info Price Trends News Exit")
+    choice = input("Select one: ")
+    choice = choice.lower()
+
+    cls()
+    if choice == "info":
+        InpStock = input("Enter ticker: ")
+        InpStock = InpStock.upper()
+        cls()
+        filecreation(InpStock)
+        Info(InpStock)
+    
+    elif choice == "price":
+        InpStock = input("Enter ticker: ")
+        InpStock = InpStock.upper()
+        print(InpStock)
+        input()
+        cls()
+        pricetracker(InpStock)
+    
+    elif choice == "trends":
+        cls()
+        trends()
+    
+    elif choice == "news":
+        news()
+
+    elif choice == "exit":
+        exit()
+
+    print("Press SPACE to continue...")
+    keyboard.wait("space") 
