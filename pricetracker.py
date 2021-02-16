688ef82467569f80a14b4834f4efa7742c8a6705
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-02-16 15:50:38 +0200

    Made price tracker function work, Now doing postandaftermarket pricetracker

--- pricetracker.py
+++ pricetracker.py
@@ -2,32 +2,38 @@ from cls import cls
 import requests
 from bs4 import BeautifulSoup
 from rich import print
-import msvcrt
 
-def pricetracker(stock):
-    
+
+
+def currentmarket(stock):
 
     url = f'https://finance.yahoo.com/quote/{stock}?p={stock}&.tsrc=fin-srch'
 
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-    while True:
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'html.parser')
+    price = soup.find("div", {"class": "D(ib) Mend(20px)"}).find_all('span')[0].text
 
-        price = soup.find("div", {"class": "D(ib) Mend(20px)"}).find_all('span')[0].text
-        
-        change = soup.find("div", {"class": "D(ib) Mend(20px)"}).find_all('span')[1]
-        
-        print(change)
-        print(change.text)
+    change = soup.find("div", {"class": "D(ib) Mend(20px)"}).find_all('span')[1]
+
+    cls()
+    value = change.text
+    bracketIndex = value.find('(')
+    result1 = value[0:bracketIndex].strip(' ')
+    result2 = value[bracketIndex:].replace('(', '').replace(
+        ')', '').replace('%', '').strip(' ')
+    print(result1)
+    changevalue = float(result1)
+    changepercent = float(result2)
+    if changevalue < 0:
+        print(f"{stock} [bold red] {changevalue} {changepercent} [/bold red]")
         input()
-        cls()
-        spanclass = "Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($negativeColor)"
-        spanreactid = "33"
-        span = "<span class=\"" + spanclass + "\" data-reactid=\""+ spanreactid +"\">" + change.text + "</span>"
-        print(span)
-        input()
-        if change == span:
-            print(f"{stock}  {price}  [bold red]{change.text}[/bold red]")
-        else:
-            print("Test")
+    elif changevalue > 0:
+        print(f"{stock} [bold green] {changevalue} ({changepercent}%) [/bold green]")
+
+def postmarketprice(stock):
+
+    url = f'https://finance.yahoo.com/quote/{stock}?p={stock}&.tsrc=fin-srch'
+
+def pricetracker(stock):
+    print("")
