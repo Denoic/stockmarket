9d648f9767f4d656d63fa14acbe9ffeeac1f1752
Kaloyan Stoyanov <kaloyangfx@gmail.com>
2021-02-21 02:26:03 +0200

    bug fixes

--- movers.py
+++ movers.py
@@ -4,14 +4,14 @@ from bs4 import BeautifulSoup
 from tabulate import tabulate
 
 def gainers():
-    res = requests.get("https://finance.yahoo.com/trending-tickers")
+    res = requests.get("https://finance.yahoo.com/gainers")
     soup = BeautifulSoup(res.content, 'lxml')
     table = soup.find_all('table')[0]
     df = pd.read_html(str(table))
     print(tabulate(df[0], headers='keys', tablefmt='psql'))
 
 def losers():
-    res = requests.get("https://finance.yahoo.com/trending-tickers")
+    res = requests.get("https://finance.yahoo.com/losers")
     soup = BeautifulSoup(res.content, 'lxml')
     table = soup.find_all('table')[0]
     df = pd.read_html(str(table))
